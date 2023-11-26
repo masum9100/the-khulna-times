ba321609285057004b3d0a4617a81a16969105d3
masum9100 <masum.seopro@gmail.com>
2023-11-27 01:46:25 +0600

    Banner added

--- src/HomePage/Home.jsx
+++ src/HomePage/Home.jsx
@@ -2,18 +2,27 @@ import React, { useEffect } from 'react';
 import HomeNews from './HomeNews';
 import HomeSubscription from './HomeSubscription';
 import HomePublisher from './HomePublisher';
+import Banner from './Banner';
+
 
 const Home = () => {
 
-    
+
     return (
-        <div className='max-w-screen-xl mx-auto lg:flex'>
-            <div className='lg:w-3/4 px-4'>
-                <HomeNews></HomeNews>
-            </div>
-            <div className='lg:w-1/4'>
-                <HomePublisher></HomePublisher>
-                <HomeSubscription></HomeSubscription>
+
+        <div>
+            <Banner></Banner>
+            <div className='max-w-screen-xl mx-auto lg:flex'>
+                <div className='lg:w-3/4 px-4'>
+                    <HomeNews></HomeNews>
+                </div>
+                <div className='lg:w-1/4'>
+                    <HomePublisher></HomePublisher>
+                    <div className='w-1/2 md:w-3/4 mx-auto my-4 bg-gray-900 h-2'>
+                        <hr />
+                    </div>
+                    <HomeSubscription></HomeSubscription>
+                </div>
             </div>
         </div>
     );
--- src/HomePage/HomePublisher.jsx
+++ src/HomePage/HomePublisher.jsx
@@ -3,23 +3,49 @@ import HomeOnePublisher from './HomeOnePublisher';
 
 const HomePublisher = () => {
     const [news, setNews] = useState([])
-    useEffect(()=>{
+    const [displayCount, setDisplayCount] = useState(3)
+    const [showLess, setShowLess] = useState(false)
+
+    useEffect(() => {
         fetch('data.json')
-        .then(res => res.json())
-        .then (data => setNews(data.slice(0, 3)))
-    },[])
+            .then(res => res.json())
+            .then(data => setNews(data.slice(0, 20)));
+    }, [])
+
+    const handleSeeMore = () => {
+        setDisplayCount(prevCount => Math.min(prevCount + 20, news.length));
+        setShowLess(true)
+    };
+
+    const handleSeeLess = () => {
+        setDisplayCount(3)
+        setShowLess(false)
+    };
+
+   
+
+
     return (
         <div>
-            <p className='font-bold text-center'>Our Honorable Publisher</p>
+            <p className='text-xl font-bold text-center my-2'>Our Honorable Publisher</p>
             {
-                news.map(item => 
-                <HomeOnePublisher
-                key={item.NewsTitle}
-                item={item}
-                ></HomeOnePublisher>)
+                news.slice(0, displayCount).map(item =>
+                    <HomeOnePublisher
+                        key={item.NewsTitle}
+                        item={item}
+                    ></HomeOnePublisher>)
             }
-            <button className='btn btn-primary'>Check All Publisher</button>
-            
+
+            {displayCount < news.length ? (
+                <button className='btn btn-primary my-2' onClick={handleSeeMore}>
+                    See More Publisher
+                </button>
+            ) : (
+                <button className='btn btn-secondary my-2' onClick={handleSeeLess}>
+                    See Less Publisher
+                </button>
+            )}
+
         </div>
     );
 };
